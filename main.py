87640f87ad8ba0c2ba97e4c021f2b6058db853fe
BurntRanch <69512353+BurntRanch@users.noreply.github.com>
2022-11-25 11:49:50 +0300

    beautifying source code

--- main.py
+++ main.py
@@ -25,66 +25,64 @@ def __process_file__(filename):
 
 def process(line, __ignore_while_loop__ = False):
     global __out_function__, __skip_until__, __while_loops__, __in_while_loop__, __force_ignore_while__
+
+    # fix indentation
     while line.startswith(' ') or line.startswith('\t'):
         line = line.removeprefix(' ').removeprefix('\t')
+
+    # check for comments or empty lines, ENDIFs or whether we should skip this code in general
     if line.startswith("$") or line == '' or line == "ENDIF;" or (__skip_until__ and (not line.endswith(__skip_until__))):
         if line.endswith("ENDIF;"):
             __skip_until__ = ""
         return
+
+    # some function stuff like recording the function code for execution
     if line == "EXIT FUNC;":
         __out_function__ = ""
         return
     if __out_function__:
         globals_pyasm[__out_function__].append(line)
         return
+    
+    # recording while loop code for execution
     if __in_while_loop__ and not line == "ENDWHILE;" and not regex.match("WHILE .*;", line) and not __ignore_while_loop__:
         __while_loops__[-1].append(line)
         return
+
+    # regex match for function calls with arguments, etc.
     if regex.match("CALL .*;", line):
         if regex.match("CALL .* ARGS \(.*\);", line):
             if " ".join(line.split(" ")[3][:-1]) == "( )":
                 sys.stdout.write("WARN: You shouldn't use CALL x ARGS (); to call a function that doesn't take an argument, use CALL x; instead.\n")
             if len(line.split(" ")) <= 3:
                 exec(f'RETURN = {line.split(" ")[1]}{line.split(" ")[3].removesuffix(";")}', globals_pyasm)
-                if not globals_pyasm['RETURN']:
-                    globals_pyasm['RETURN'] = "None"
             else:
                 exec(f'RETURN = {line.split(" ")[1]}{" ".join(line.split(" ")[3:]).removesuffix(";")}', globals_pyasm)
-                if not globals_pyasm['RETURN']:
-                    globals_pyasm['RETURN'] = "None"
         else:
             if isinstance(globals_pyasm.get("".join(line.split(" ")[1]).removesuffix(";"), None), list):
                 for i in globals_pyasm.get("".join(line.split(" ")[1]).removesuffix(";"), None):
                     process(i)
             else:
                 exec(f'RETURN = {"".join(line.split(" ")[1]).removesuffix(";")}()', globals_pyasm)
+    # variable manipulation (we have a sandbox so now we dont have to worry about users overwriting runtime vars!)
     elif regex.match("SET .* TO .*;", line):
-        if line.split(" ")[1] != 'RETURN' and not regex.match("__.*__", line.split(" ")[1]):
-            exec(f'{line.split(" ")[1]} = {" ".join(line.split(" ")[3:]).removesuffix(";")}', globals_pyasm)
-        else:
-            raise AccessError(line + ", You can't set PyAsm runtime variables.")
+        exec(f'{line.split(" ")[1]} = {" ".join(line.split(" ")[3:]).removesuffix(";")}', globals_pyasm)
     elif regex.match("INCREMENT .* BY .*;", line):
         if line.split(" ")[-1].removesuffix(';').isnumeric():
-            if not regex.match("__.*__", line.split(" ")[1]):
-                globals_pyasm[line.split(" ")[1]] += int(line.split(" ")[-1].removesuffix(';'))
-            else:
-                raise AccessError("'" + line + "', You can't set PyAsm runtime variables.")
+            globals_pyasm[line.split(" ")[1]] += int(line.split(" ")[-1].removesuffix(';'))
     elif regex.match("DECREMENT .* BY .*;", line):
         if line.split(" ")[-1].removesuffix(';').isnumeric():
-            if not regex.match("__.*__", line.split(" ")[1]):
-                globals_pyasm[line.split(" ")[1]] -= int(line.split(" ")[-1].removesuffix(';'))
-            else:
-                raise AccessError("'" + line + "', You can't set PyAsm runtime variables.")
+            globals_pyasm[line.split(" ")[1]] -= int(line.split(" ")[-1].removesuffix(';'))
+    # memory management
     elif regex.match("DELETE VAR .*;", line):
         del globals_pyasm[line.split(" ")[2].removesuffix(";")]
     elif regex.match("DELETE FUNC .*;", line):
         del globals_pyasm[line.split(" ")[2].removesuffix(";")]
+    # function definition, it just records the code so we can evaluate each line eventually
     elif regex.match("DEFINE FUNC .*;", line):
-        if line.split(" ")[2].removesuffix(";") != 'RETURN' and not regex.match("__.*__", line.split(" ")[2].removesuffix(";")):
-            __out_function__ = line.split(" ")[2].removesuffix(";")
-            globals_pyasm[line.split(" ")[2].removesuffix(";")] = []
-        else:
-            raise AccessError("'" + line + "' You can't set PyAsm runtime variables.")
+        __out_function__ = line.split(" ")[2].removesuffix(";")
+        globals_pyasm[__out_function__] = []
+    # importing
     elif regex.match("INCLUDE .*;", line):
         if regex.match("INCLUDE .* FROM .*;", line):
             exec(f'from {line.split(" ")[3].removesuffix(";")} import {line.split(" ")[1]}', globals_pyasm)
@@ -93,20 +91,23 @@ def process(line, __ignore_while_loop__ = False):
                 __process_file__(f'{line.split(" ")[1].removesuffix(";")}.pym')
             else:
                 exec(f'import {line.split(" ")[1].removesuffix(";")}', globals_pyasm)
+    # ah yes, if statements.
     elif regex.match("IF .*;", line):
         __if_return__ = eval(f'not not {" ".join(line.split(" ")[1:]).removesuffix(";")}', globals_pyasm)
         if not __if_return__:
             __skip_until__ = "ELSE;"
-    elif regex.match("WHILE .*;", line):
-        __while_statements__.append(" ".join(line.split(" ")[1:]).removesuffix(";"))
-        __while_loops__.append([])
-        __in_while_loop__ = True
-        __force_ignore_while__ = True
     elif line == "ELSE;":
         if __if_return__:
             __skip_until__ = "ENDIF;"
         else:
             __skip_until__ = ""
+    # while loops
+    elif regex.match("WHILE .*;", line):
+        __while_statements__.append(" ".join(line.split(" ")[1:]).removesuffix(";"))
+        __while_loops__.append([])
+        __in_while_loop__ = True
+        __force_ignore_while__ = True
+    
     elif __in_while_loop__ and line == "ENDWHILE;" and not __ignore_while_loop__:
         __dont_eval__ = False
         for __WHILE_STATEMENT__ in __while_statements__[:-1]:
@@ -119,9 +120,12 @@ def process(line, __ignore_while_loop__ = False):
         __while_loops__.pop()
         __while_statements__.pop()
         __in_while_loop__ = not not __while_loops__
+    # should this code be deleted? i dont think so..
     elif line == __skip_until__:
         __skip_until__ = ""
     else:
+        # TO-DO:
+        # - use another error instead of this, this is for python.
         raise SyntaxError(line)
 
 # check if a file is supplied
