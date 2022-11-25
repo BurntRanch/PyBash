2e8f081e2529b529325e9972ebce6230b874294f
BurntRanch <69512353+BurntRanch@users.noreply.github.com>
2022-11-25 19:18:50 +0300

    last refactor i swear

--- helpers/pyasm_errors.py
+++
@@ -1,9 +0,0 @@
-
-
-class AccessError(Exception):
-    """ Raised when a user tries to set PyAsm runtime variables """
-    pass
-
-class NoImportFound(Exception):
-    """ Raised when a user tries to import a nonexistent library """
-    pass
